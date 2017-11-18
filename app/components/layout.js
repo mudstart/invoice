35120dfc49bbec3edf9b8da52a726190d9ece2f4
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-11-18 14:36:03 -0400

    add events and contacts

--- app/components/layout.js
+++ app/components/layout.js
@@ -4,12 +4,34 @@ import Contact from './Contact';
 import data from './data';
 
 class Layout extends React.Component {
+  componentWillMount() {
+    this.setState({
+      contacts: data,
+    });
+  }
+
+  addContact = (e) => {
+    e.preventDefault();
+
+    const contacts = this.state.contacts;
+    const newId = contacts[contacts.length - 1].id + 1;
+
+    this.setState({
+      contacts: contacts.concat({ id: newId,
+        name: 'New Contact ${newId}',
+        email: '${newId}@example.com' }),
+    });
+  }
+
   render() {
     return (
-      <div id='Layout' className='pure-g'>
-        {data.map(info =>
-          <Contact key={info.id} {...info} />
-        )}
+      <div id='Layout'>
+        <a href='#' className='pure-button' onClick={this.addContact}>Add Contact</a>
+        <div className='pure-g'>
+          {this.state.contacts.map(info =>
+            <Contact key={info.id} {...info} />
+          )}
+        </div>
       </div>
     );
   }
