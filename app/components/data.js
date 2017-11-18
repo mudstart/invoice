013bddb5e0e1c990a7f5d43e940061faf3df5b41
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-11-18 14:08:57 -0400

    cleaning layout

---
+++ app/components/data.js
@@ -0,0 +1,6 @@
+const data = [
+  { id: 1, name: 'Jose Maduro', email: 'maduro@gmail.com' },
+  { id: 2, name: 'Pedro Perez', email: 'perez@gmail.com' },
+  { id: 3, name: 'Maria Abreu', email: 'abreu@gmail.com' },
+];
+export default data;
