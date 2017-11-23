473cdb8823848b06bfa62d433e904bf1130cdb88
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-11-23 15:20:05 -0400

    setup loging member and gest

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import classNames from 'classnames';
-import styles from './Layout.sass';
 import { Link } from 'react-router';
+import { observer, inject } from 'mobx-react';
 
-const Layout = props =>
-  <div id='Layout' className={styles.layout}>
-    <div className={classNames('pure-menu', 'pure-menu-horizontal', 'pure-menu-fixed', styles.mainNav)}>
-      <Link to='/' className={classNames('pure-menu-heading', styles.heading)}>Invoiced</Link>
-      <ul className='pure-menu-list'>
-          <li className='pure-menu-item'>
-            <Link to='/users/sign_in' className={classNames('pure-menu-link', styles.link)}>Sign In</Link>
-          </li>
-      </ul>
-    </div>
-{props.children}
-</div>;
+import Guest from './Guest';
+import Member from './Member';
+import styles from './index.sass';
 
-  export default Layout;
+@inject('user') @observer
+class Application extends React.Component {
+  guessOrMember(){
+    const { user } = this.props;
+
+    if (user.signedIn) {
+      return (<Member />);
+    }
+      return (<Guest/>);
+  }
+
+  render(){
+    return(
+      <div id='Layout' className={styles.layout}>
+        <div className={classNames('pure-menu', 'pure-menu-horizontal', 'pure-menu-fixed', styles.mainNav)}>
+          <Link to='/' className={classNames('pure-menu-heading', styles.heading)}>Invoiced</Link>
+          {this.guessOrMember()}
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+
+export default { Application };
