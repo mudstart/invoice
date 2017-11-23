6bec976ad5f8d782810bd1107bb642b4af002c79
Jose Miguel Maduro <maduro.josem@gmail.com>
2017-11-22 20:12:00 -0400

    call api

--- app/components/Collection.js
+++ app/components/Collection.js
@@ -6,19 +6,21 @@ import styles from './Collection.sass';
 
 @observer(['contacts'])
 class Layout extends React.Component {
+  componentWillMount() {
+    this.props.contacts.fetchAll();
+  }
+
   addContact = (e) => {
     e.preventDefault();
 
-    const contacts = this.props.contacts.all.slice();
-    const newId = contacts[contacts.length - 1].id + 1;
-
     this.props.contacts.add({
-      id: newId,
-      name: this.refs.name.value,
+      first_name: this.refs.first_name.value,
+      last_name: this.refs.last_name.value,
       email: this.refs.email.value,
     });
 
-    this.refs.name.value = null;
+    this.refs.first_name.value = null;
+    this.refs.last_name.value = null;
     this.refs.email.value = null;
   };
 
@@ -28,8 +30,10 @@ class Layout extends React.Component {
         <form className='pure-form' onSubmit={this.addContact}>
           <fieldset>
             <legend>New Contact</legend>
-            <input ref='email' type="email" placeholder="example@example.com" />
-            <input ref='name' type="text" placeholder="Name" />
+            <input ref='email' type="email" placeholder="exmple@exmple.com" />
+            <input ref='first_name' type="text" placeholder="First Name" />
+            <input ref='last_name' type="text" placeholder="Last Name" />
+
             <button type="submit" className='pure-button pure-button-primary'>Add</button>
           </fieldset>
         </form>
